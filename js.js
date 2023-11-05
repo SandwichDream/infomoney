8b700c88c3fed248dde2e0e79973081bdf2da1ba
SandwichDream <karnll.adnitorll@gmail.com>
2023-11-05 16:13:14 +0300

    c3

--- js.js
+++ js.js
@@ -1,14 +1,34 @@
 window.onload = () => {
     let btnSign = document.querySelector(".btn-sign");
-    let btnCross = document.querySelector(".btn-cross");
-    let signIn = document.querySelector(".sign-in");
+    let btnLog = document.querySelector(".btn-log");
+    let modalWindow = document.querySelector(".modal-window");
 
+    let signIn = document.querySelector(".sign-in");
+    let btnCrossSign = document.querySelector(".sign-in>.btn-cross");
+    let logIn = document.querySelector(".log-in");
+    let btnCrossLog = document.querySelector(".log-in>.btn-cross");
+
+    // Sign
     btnSign.addEventListener("click", function(e){
         e.preventDefault();
+        modalWindow.classList.remove("d-none");
         signIn.classList.remove("d-none");
     });
-    btnCross.addEventListener("click", function(e){
+    btnCrossSign.addEventListener("click", function(e){
         e.preventDefault();
+        modalWindow.classList.add("d-none");
         signIn.classList.add("d-none");
+    });
+
+    // Log
+    btnLog.addEventListener("click", function(e){
+        e.preventDefault();
+        modalWindow.classList.remove("d-none");
+        logIn.classList.remove("d-none");
+    });
+    btnCrossLog.addEventListener("click", function(e){
+        e.preventDefault();
+        modalWindow.classList.add("d-none");
+        logIn.classList.add("d-none");
     });
 }
