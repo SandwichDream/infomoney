1c4f64407e99e3571b00d63f014e489150eea396
SandwichDream <karnll.adnitorll@gmail.com>
2023-11-06 15:59:38 +0300

    c4

---
+++ demo/src/test/java/com/infomoney/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infomoney.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
